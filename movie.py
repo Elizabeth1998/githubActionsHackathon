bad58bafb3ad7aaf7814ad9b441532786155300b
Elizabeth1998 <31674423+Elizabeth1998@users.noreply.github.com>
2020-08-31 19:55:46 -0400

    Adding python file

---
+++ movie.py
@@ -0,0 +1,29 @@
+import random
+
+random_movie = "blank state"
+
+value = random.randint(0,9)
+
+if value == 0:
+    random_movie = "Bill & Ted's Excellent Adventure"
+elif value == 1:
+    random_movie = "Die Hard"
+elif value == 2:
+    random_movie = "Can't Buy Me Love"
+elif value == 3:
+    random_movie = "The Lost Boys"
+elif value == 4:
+    random_movie = "Beetlejuice"
+elif value == 5:
+    random_movie = "The Goonies"
+elif value == 6:
+    random_movie = "The Breakfast Club"
+elif value == 7:
+    random_movie = "Sixteen Candles"
+elif value == 8:
+    random_movie = "Short Circuit"
+elif value == 9:
+    random_movie =  "Weekend at Bernies"
+else:
+    print("try again")
+
